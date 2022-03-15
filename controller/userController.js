8350bd1ff6b71cc5cf4e292034cb8f2b82413821
kongtaemin <rhdxoals@gmail.com>
2022-03-16 01:54:01 +0900

    user CRUD API 완료

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,65 @@
 import pool from "../db/database.js";
+import { CheckValidator } from "../util/vailidator.js";
+
+/**
+ * TODO: 유효성 검사 필요하면 추가
+ * if (CheckValidator(req, res)) return;
+ * */
 
 export async function getUserById(req, res) {
   const id = req.params.id;
-  const sql = `SELECT * FROM user WHERE id=${id}`;
-  const result = await pool.query(sql);
+  let result;
+  try {
+    const sql = `SELECT * FROM user WHERE id=${id}`;
+    result = await pool.query(sql);
+  } catch (e) {
+    if (e) return res.json(e.message);
+  }
+
   res.status(200).json(result[0][0]);
+}
+
+export async function createUser(req, res) {
+  if (CheckValidator(req, res)) return;
+
+  try {
+    const sql = `INSERT INTO user (email, password, name, birth, job) VALUES ('${req.body.email}', '${req.body.password}', '${req.body.name}', '${req.body.birth}', '${req.body.job}')`;
+    const result = await pool.query(sql);
+  } catch (e) {
+    if (e) return res.json(e.message);
+  }
+
+  res.status(200).json({ res: "success", msg: "유저 생성 성공" });
+}
+
+export async function updateUser(req, res) {
+  if (CheckValidator(req, res)) return;
+
+  try {
+    const sql = `UPDATE user SET email = '${req.body.email}', password = '${req.body.password}', name = '${req.body.name}', birth = '${req.body.birth}', job = '${req.body.job}' WHERE (id = '${req.body.id}');
+    `;
+    const result = await pool.query(sql);
+  } catch (e) {
+    if (e) return res.json(e.message);
+  }
+
+  res.status(200).json({ res: "success", msg: "유저정보 업데이트 성공" });
+}
+
+export async function deleteUser(req, res) {
+  let affectiveRows;
+  try {
+    const sql = `DELETE FROM user WHERE (id = '${req.params.id}')`;
+    const result = await pool.query(sql);
+    affectiveRows = result[0].affectedRows;
+  } catch (e) {
+    if (e) return res.json(e.message);
+  }
+
+  if (affectiveRows == 0)
+    return res
+      .status(400)
+      .json({ res: "fail", msg: "유저가 존재하지 않습니다." });
+
+  res.status(200).json({ res: "success", msg: "유저 삭제 성공" });
 }
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,20 +1,44 @@
 import express from "express";
-import { getUserById } from "../controller/userController.js";
+import {
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controller/userController.js";
+import { body, check } from "express-validator";
+
+// validator 와 controller 로만 구성
 
 const router = express.Router();
 
 router.get("/:id", getUserById);
 
-router.post("/", (req, res) => {
-  res.send("post user");
-});
+router.post(
+  "/",
+  [
+    body(["id", "email", "password", "password_check", "name", "birth", "job"])
+      .notEmpty()
+      .trim(),
+    check("email").isEmail(),
+    body("password").custom((value, { req }) => {
+      if (value !== req.body.password_check) {
+        throw new Error("비밀번호가 일치하지 않습니다");
+      }
+      return true;
+    }),
+  ],
+  createUser
+);
 
-router.put("/", (req, res) => {
-  res.send("put user");
-});
+router.put(
+  "/",
+  [
+    body(["email", "password", "name", "birth", "job"]).notEmpty().trim(),
+    check("email").isEmail(),
+  ],
+  updateUser
+);
 
-router.delete("/", (req, res) => {
-  res.send("delete user");
-});
+router.delete("/:id", deleteUser);
 
 export default router;
