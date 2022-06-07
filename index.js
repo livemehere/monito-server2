deb63ba3acc11de439b4e06c25265bc62a6869f7
Ubuntu <ubuntu@ip-172-31-47-229.ap-northeast-2.compute.internal>
2022-06-07 05:51:23 +0000

    ssl 인증완료;

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 import userRouter from "./router/userRouter.js";
 import signInRouter from "./router/signInRouter.js";
@@ -14,6 +17,16 @@ import roomRouter from "./router/roomRouter.js";
 import userRoomRouter from "./router/userRoomRouter.js";
 
 const app = express();
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/monito.ml/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/monito.ml/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/monito.ml/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use(cors(config.corsOptions));
 app.use(express.json());
@@ -35,8 +48,15 @@ app.use('/calendar',calendarRouter);
 app.use('/room',roomRouter);
 app.use('/userRoom',userRoomRouter);
 
-app.listen(config.port, () => {
-  console.log(`server is running on ${config.port}`);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3000, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(3333, () => {
+	console.log('HTTPS Server running on port 443');
 });
 
 // 404
